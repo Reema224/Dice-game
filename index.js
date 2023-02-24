f0b20ebb0570ede297139796b7e5e32780d5fe6c
rima <rima.abouchakra.gms@gmail.com>
2023-02-24 22:31:48 +0200

    added emoji image with javascript

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 window.onload = (event) =>{
 
 function randomDice(){
-var x=Math.floor(Math.random() * 6) + 1;
-var y=Math.floor(Math.random() * 6) + 1;
- document.getElementById("first").src=`figures/Dice_${x}.png`;
-document.getElementById("second").src=`figures/Dice_${y}.png`;
-if (x > y) {
-  document.getElementById("header").innerHTML = "Player 1 Wins!";
-} else if (y > x) {
-   document.getElementById("header").innerHTML = "Player 2 Wins!";
-} else {
-   document.getElementById("header").innerHTML = "Draw!";
-}
+   var x=Math.floor(Math.random() * 6) + 1;
+   var y=Math.floor(Math.random() * 6) + 1;
+   document.getElementById("first").src=`figures/Dice_${x}.png`;
+   document.getElementById("second").src=`figures/Dice_${y}.png`;
+   var emoji = document.createElement("img");
+   emoji.src='figures/emoji.png';
+     if (x > y) {
+        document.getElementById("faceLeft").appendChild(emoji); 
+        document.getElementById("header").innerHTML = "Player 1 Wins!";
+        
+     } else if (y > x) {
+        document.getElementById("header").innerHTML = "Player 2 Wins!";
+        document.getElementById("faceRight").appendChild(emoji); 
+     } else {
+        document.getElementById("header").innerHTML = "Draw!";
+     }
+    
+
+    
 }
 randomDice();
-};
+
+}; 
